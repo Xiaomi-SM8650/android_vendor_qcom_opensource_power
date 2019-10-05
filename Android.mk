167988784e34ae99b1685d26606c589dddff7771
Michael Bestas <mkbestas@lineageos.org>
2019-10-06 01:34:17 +0300

    power: Clean up and fix set_interactive_override
    * Remove debug logs and useless comments
    * Consistent return values
    * Check if set_interactive_override handled hints and print
      info message if it didn't
    
    Change-Id: Iff9f7327ba71c282e34ff3143c45a3f40f72fc92
    Signed-off-by: chandu078 <chandudyavanapelli03@gmail.com>

--- Android.mk
+++ Android.mk
@@ -59,10 +59,6 @@ ifeq ($(call is-board-platform-in-list,sdm845), true)
 LOCAL_SRC_FILES += power-845.c
 endif
 
-ifeq ($(call is-board-platform-in-list,sdm710), true)
-LOCAL_SRC_FILES += power-710.c
-endif
-
 ifeq ($(call is-board-platform-in-list,trinket), true)
 LOCAL_SHARED_LIBRARIES := liblog libcutils libdl libxml2
 LOCAL_SRC_FILES := power.c metadata-parser.c utils.c list.c hint-data.c powerhintparser.c
